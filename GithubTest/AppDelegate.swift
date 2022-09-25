42fc5c71af7db2f9cecdf20f7e4fb2bb7b35a694
cylinderheadgasket <aozcelik.4378@gmail.com>
2022-09-25 12:57:19 +0300

    Initial commit for GithubTest

--- GithubTest/AppDelegate.swift
+++ GithubTest/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
+    //GithubTest
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
